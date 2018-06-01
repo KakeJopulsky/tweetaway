df2d2da6f6b1ef4fe2edd39b57d0f15242a5acb7
Jake Kopulsky <jakekopulsky@gmail.com>
2018-05-31 22:45:02 -0700

    export db methods to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,16 @@
+const { insert, remove, getOne, getAll } = require('../database/db.js');
+const bodyParser = require('body-parser');
 const express = require('express');
 const path  = require('path');
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, 'dist')));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
 // ROUTES
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send('Got a GET request!')
+});
+app.post('/', (req, res) => {
+  res.send('Got a POST request')
+});
