a95ebcd5027cbd4d9662e90c8958a91e399d0726
Jake Kopulsky <jakekopulsky@gmail.com>
2018-06-07 15:19:45 -0700

    edit tweet progress

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,14 @@ class App extends React.Component {
       charCount: 280,
       date: Date.now(),
       tweets: null,
+      editMode: false,
+      edittedTweetID: null,
     }
     this.handleChange = this.handleChange.bind(this);
-    this.handleInput = this.handleInput.bind(this);
     this.handleDate = this.handleDate.bind(this);
+    this.postTweet = this.postTweet.bind(this);
+    this.editTweet = this.editTweet.bind(this);
+    this.getTweets = this.getTweets.bind(this);
 	}
 
 	componentDidMount() {
@@ -42,11 +46,15 @@ class App extends React.Component {
     })
   };
 
-  handleInput() {
+  postTweet() {
     axios.post('/post/tweet', { message: this.state.inputText, date: this.state.date })
       .then((res) => console.log(res))
       .catch((err) => console.log(err))
-    this.setState({ inputText: '' })
+    this.setState({ inputText: '', editMode: false })
+    this.getTweets();
+  };
+
+  getTweets() {
     axios.get('/get/tweets')
       .then(({ data }) => this.setState({ tweets: data }))
   };
@@ -56,23 +64,31 @@ class App extends React.Component {
     this.setState({ date: UTCdate });
   };
 
+  editTweet(tweet) {
+    console.log(tweet);
+    this.setState({ editMode: true, inputText: tweet.message, edittedTweetID: tweet._id })
+    // this.setState({ editMode: true, inputText: tweet.message })
+    // if true, have some flag in <Tweet /> notifying user
+  }
+
 	render() {
-		const isLoggedIn = this.state.isLoggedIn;
+    const isLoggedIn = this.state.isLoggedIn;
+    
 		return (
 			<div className="container">
         {isLoggedIn 
           ? (
               <Tabs defaultActiveKey={1} animation={false} id="noanim-tab-example">
                 <Tab eventKey={1} title="Tweet">
-                <Tweet user={this.state.isLoggedIn} 
-                       input={this.handleChange} 
-                       submit={this.handleInput} 
-                       textValue={this.state.inputText}
-                       handleDate={this.handleDate}
-                />
+                  <Tweet user={this.state.isLoggedIn}
+                        input={this.handleChange} 
+                        submit={this.postTweet} 
+                        textValue={this.state.inputText}
+                        handleDate={this.handleDate}
+                  />
                 </Tab>
                 <Tab eventKey={2} title="Queue">
-                  <Queue tweets={this.state.tweets}/>
+                  <Queue tweets={this.state.tweets} editTweet={this.editTweet} />
                 </Tab>
               </Tabs>
             )
